145779dabb23ce26c7801b98dea703f63362259c
Jonas Almeida <jalmeida@mathbiol.org>
2019-12-15 20:25:38 -0500

    msg

--- epibox.js
+++ epibox.js
@@ -9,12 +9,13 @@ epibox=async function(){
 
 epibox.login=async function(){
     // look for token infoin localStorage
-    if(localStorage.epiboxtoken){epibox.oauth.token=JSON.parse(localStorage.epiboxtoken)}
+    if(localStorage.epiboxtoken){epibox.oauth.token=JSON.parse(localStorage.epiboxtoken)} // remove if localStorage persistence undesirable
     if(epibox.parms.code){ // dance first step taken
         epibox.oauth.token = await (await fetch('https://api.box.com/oauth2/token',{
             method:"POST",
             body:`grant_type=authorization_code&code=${epibox.parms.code}&client_id=${epibox.oauth.client_id}&client_secret=${epibox.oauth.secret}`
         })).json()
+        epibox.oauth.token.created_at=Date.now()
     }
     if(!epibox.oauth.token){ // start dance
         location.href=`https://account.box.com/api/oauth2/authorize?client_id=${epibox.oauth.client_id}&response_type=code&redirect_uri=${location.origin+location.pathname}`
@@ -23,8 +24,9 @@ epibox.login=async function(){
             localStorage.epiboxtoken=JSON.stringify(epibox.oauth.token)
             location.href=location.origin+location.pathname
         }else{
-            delete localStorage.epiboxtoken
-            alert('logged in')
+            //delete localStorage.epiboxtoken
+            //alert('logged in')
+            epibox.msg(`> logged in session started at ${new Date(epibox.oauth.token.created_at)}`,'green')
         }
     }
 }
@@ -49,7 +51,7 @@ epibox.getOauth=function(uri=location.origin){
     }
 }
 
-epibox.readParms=function(str=location.hash+location.search){
+epibox.readParms=function(str=location.search){ // by default reads search only, for hash as well one could do (location.hash+location.search)
     str=str.replace(/[\?\#]/g,'&').replace(/^\&/,'')
     //epibox.parms=JSON.parse(localStorage.epibox||'{}')
     epibox.parms={}
@@ -58,4 +60,21 @@ epibox.readParms=function(str=location.hash+location.search){
         epibox.parms[aa[0]]=aa[1]
     })
     //localStorage.epibox=JSON.stringify(epibox.parms)
+}
+
+epibox.msg=function(hm,color="blue",dt=20){
+    console.log(hm)
+    let msg = document.getElementById('msg')
+    if(msg){
+        msg.style.color=color
+        msg.innerHTML=''
+        if(epibox.msg.t){clearInterval(epibox.msg.t)} // interrupt previous message if not completed
+        let i=0
+        epibox.msg.t=setInterval(_=>{
+            //msg.innerHTML+=hm[i]
+            msg.innerHTML=hm.slice(0,i)
+            i++
+            if(i>hm.length){clearInterval(epibox.msg.t)}
+        },dt)
+    }
 }
