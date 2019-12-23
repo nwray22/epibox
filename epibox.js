fe9aed565621e381d0aa641dfa057e4be762a5f5
Jonas Almeida <jalmeida@mathbiol.org>
2019-12-22 20:19:35 -0500

    refresh stale token

--- epibox.js
+++ epibox.js
@@ -115,6 +115,9 @@ epibox.checkToken= async function(){ // check token, refresh if needed
     if(epibox.oauth.token.refresh_token){
         epibox.oauth.token.initiated_at=epibox.oauth.token.initiated_at||epibox.oauth.token.created_at
         epibox.msg(`> oauth session active,\n initiated at ${new Date(epibox.oauth.token.initiated_at)},\n last refreshed at ${new Date(epibox.oauth.token.created_at)}.`)
+        if(Date.now()-(epibox.oauth.token.created_at+epibox.oauth.token.expires_in*1000-10000)>0){ // refresh at 10secs before expiration
+            await epibox.refreshToken()
+        }
     }else{
         epibox.msg('> refresh token missing, please restart session','red')
     }
